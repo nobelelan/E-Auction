8cc8c7f01494453bcab01c108d8baec60c6ac722
Nobel Elan <nobelelan@gmail.com>
2023-06-08 08:24:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/e_waste/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.e_waste
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.RequiresApi
+import com.example.e_waste.databinding.ActivityMainBinding
+import com.example.e_waste.utils.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null)
+            setUpBottomNav()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        setUpBottomNav()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setUpBottomNav() {
+        val graphIds = listOf(
+            R.navigation.home_nav,
+            R.navigation.fav_nav,
+            R.navigation.cart_nav,
+            R.navigation.profile_nav
+        )
+        binding.bottomNav.setupWithNavController(
+            graphIds,
+            supportFragmentManager,
+            R.id.nav_host_fragment,
+            intent
+        )
+    }
+}
---
+++ app/src/main/java/com/example/e_waste/ui/profile/ProfileFragment.kt
@@ -0,0 +1,36 @@
+package com.example.e_waste.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_waste.R
+import com.example.e_waste.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentProfileBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}

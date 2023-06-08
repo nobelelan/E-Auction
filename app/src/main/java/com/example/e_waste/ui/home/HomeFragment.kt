929dee9bc6f1c44a0d063ad28284be388c2cf81b
Nobel Elan <nobelelan@gmail.com>
2023-06-08 23:07:38 +0600

    image slider added

--- app/src/main/java/com/example/e_waste/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/e_waste/ui/home/HomeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.constants.AnimationTypes
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.e_waste.R
 import com.example.e_waste.databinding.FragmentHomeBinding
 
@@ -25,6 +29,34 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentHomeBinding.bind(view)
+
+        setUpImageSlider()
+
+        binding.imageSlider.setItemClickListener(object: ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun setUpImageSlider() {
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.lost_something))
+        imageList.add(SlideModel(R.drawable.findt_it))
+        imageList.add(SlideModel(R.drawable.found_something))
+        imageList.add(SlideModel(R.drawable.post_it))
+
+        binding.imageSlider.apply {
+            setImageList(imageList, ScaleTypes.CENTER_CROP)
+            setSlideAnimation(AnimationTypes.DEPTH_SLIDE)
+            startSliding(2000) // with new period
+//            startSliding()
+//            stopSliding()
+        }
     }
 
     override fun onDestroyView() {

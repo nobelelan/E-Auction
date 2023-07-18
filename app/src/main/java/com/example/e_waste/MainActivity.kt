eeb0103027d4d8633c671e495aeb520e74dee49a
Nobel Elan <nobelelan@gmail.com>
2023-07-18 21:42:22 +0600

    hamburger works good

--- app/src/main/java/com/example/e_waste/MainActivity.kt
+++ app/src/main/java/com/example/e_waste/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import com.example.e_waste.databinding.ActivityMainBinding
 import com.example.e_waste.utils.ExtensionFunctions.showToast
@@ -14,11 +15,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var toggle: ActionBarDrawerToggle
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         if (savedInstanceState == null)
             setUpBottomNav()
